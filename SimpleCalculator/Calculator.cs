f57815d8454baf59321373a121da5687625c153d
lmleemhuis <lmleemhuis@gmail.com>
2021-08-31 19:57:45 -0400

    lml: Initial code for simple calculator exercise.

---
+++ SimpleCalculator/Calculator.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCalculator
+{
+    public class Calculator
+    {
+        private Double m_total; //The total number; we are performing actions to this number (e.g. adding subtracting, etc to this number)
+        private string m_value; //The number we are adding/multiplying/etc to m_total; 
+                                //will get turned into a double; should be able to accommdate only 1 period in the string
+        private Sign m_sign; //Whether or not m_value is currently positive or negative
+
+        /**************************************
+         * Indicates whether or not a number is positive or negative
+         *************************************/
+        public enum Sign
+        {
+            Plus=0,
+            Minus=1
+        }
+
+        /**************************************
+         * Sets up calculator object.  All values initialize at 0
+         *************************************/
+        public Calculator()
+        {
+            m_total = 0;
+            m_value = "";
+        }
+
+        /**************************************
+         * Getter for the calculated total
+         *************************************/
+        public double getTotal()
+        {
+            return m_total;
+        }
+
+        /**************************************
+         * Getter for m_value as a double
+         *************************************/
+        public double getValue()
+        {
+            double val = 0;
+            try
+            {
+                val = getDoubleValue();
+            }
+            catch ( Exception exc)
+            {
+                throw exc;
+            }
+
+            return val;
+        }
+
+        /**************************************
+         * Getter for a string concatentation of sign and value; can be used for display purposes
+         *************************************/
+        public string getDisplayValue()
+        {
+            return getSignString() + m_value;
+        }
+
+        /**************************************
+         * Clears internal total and value, and returns the current total
+         * Return total value
+         *************************************/
+        public double clear()
+        {
+            m_total = 0;
+            m_value = "";
+            m_sign = Sign.Plus;
+
+            return m_total;
+        }
+
+        /**************************************
+         * Clears internal value, and returns the current value
+         * Return total value
+         *************************************/
+        public double clearValue()
+        {
+            m_value = "";
+            m_sign = Sign.Plus;
+
+            return getDoubleValue();
+        }
+
+        /**************************************
+         * Toggles negative or positive for value
+         * Return: current value
+         *************************************/
+        public string toggleSign()
+        {
+            string valueModifier = "";
+            if(m_sign == Sign.Plus)
+            {
+                m_sign = Sign.Minus;
+                valueModifier = "-";
+            }
+            else if(m_sign == Sign.Minus)
+            {
+                m_sign = Sign.Plus;
+            }
+
+            return valueModifier + m_value;
+        }
+
+        /**************************************
+         * Concatenates another number (or decimal) to the end of value
+         * On error, throws FormatException or InvalidCastException
+         * Return: current value
+         *************************************/
+        public string appendToValue(string appendVal)
+        {
+            string valToAdd = "";
+            if(appendVal == ".")
+            {
+                //don't add a decimal if one already exists in the value
+                //This shouldn't occur because we should disable the decimal button if they push it twice,
+                //but add the logic just in case
+                if (!m_value.Contains('.'))
+                {
+                    valToAdd = appendVal;
+                }
+                else
+                {
+                    throw new FormatException("<" + appendVal + "> cannot be appended because the value already contains a decimal.");
+                }
+            }
+            else
+            {
+                if(!Int32.TryParse(appendVal, out int result))
+                {
+                    //only accept integer numbers, not strings.  Decimals handled higher up.
+                    throw new InvalidCastException("<" + appendVal + "> is an invalid integer and cannot be added to the value.");
+                }
+                else
+                {
+                    valToAdd = appendVal;
+                }
+
+            }
+            m_value += valToAdd;
+
+            return getSignString() + m_value;
+        }
+
+        /**************************************
+         * Converts value from a string to a double, if possible
+         * On error, throws InvalidCastException
+         * Return: m_value as a double
+         *************************************/
+        private double getDoubleValue()
+        {
+           if(m_value == "")
+            {
+                return 0;
+            }
+
+            if (!Double.TryParse(getSignString() + m_value, out double valDouble))
+            {
+                throw new InvalidCastException("<" + m_value + "> is an invalid double.  Operations cannot be performed!");
+            }
+
+            return valDouble;
+        }
+
+        /**************************************
+         * Converts m_sign to - or emptystring for use in operational logic
+         * Return: "-" if m_value's sign is negative, otherwise empty string
+         *************************************/
+        private string getSignString()
+        {
+            string sign = "";
+            if (m_sign == Sign.Minus)
+            {
+                sign = "-";
+            }
+
+            return sign;
+        }
+
+        /**************************************
+         * Multiplies the total number by the value
+         * Returns: The new total after the multiply operation is complete
+         *************************************/
+        public double multiply()
+        {
+            try
+            {
+                m_total *= getDoubleValue();
+                m_value = "";
+            }
+            catch (Exception exc)
+            {
+                //there is no custom text for overflow exceptions, but they should be thrown
+                throw exc;
+            }
+
+            return m_total;
+        }
+
+        /**************************************
+         * Divides the total number by the value
+         * Returns: The new total after the divide operation is complete
+         *************************************/
+        public double divide()
+        {
+            try 
+            { 
+                m_total /= getDoubleValue();
+                m_value = "";
+            }
+            catch (Exception exc)
+            {
+                //there is no custom text for overflow exceptions, but they should be thrown
+                throw exc;
+            }
+
+            return m_total;
+        }
+
+        /**************************************
+         * Adds the value to the total
+         * Returns: The new total after the add operation is complete
+         *************************************/
+        public double add()
+        {
+            try
+            {
+                m_total += getDoubleValue();
+                m_value = "";
+            }
+            catch (Exception exc)
+            {
+                //there is no custom text for overflow exceptions, but they should be thrown
+                throw exc;
+            }
+
+            return m_total;
+        }
+
+        /**************************************
+         * Subtracts the value from the total
+         * Returns: The new total after the subtract operation is complete
+         *************************************/
+        public double subtract()
+        {
+            try
+            { 
+                m_total -= getDoubleValue();
+                m_value = "";
+            }
+            catch (Exception exc)
+            {
+                //there is no custom text for overflow exceptions, but they should be thrown
+                throw exc;
+            }
+
+            return m_total;
+        }
+
+
+    }
+
+   
+}
---
+++ SimpleCalculator/MainWindow.xaml.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculator m_calc;
+        public MainWindow()
+        {
+            InitializeComponent();
+            TextboxTotalDisplay.Text = "0";
+            TextboxValueDisplay.Text = "0";
+            m_calc = new Calculator();
+        }
+
+        /**************************************
+         * Handler for any button numeric or decimal button push; appends a number/decimal to the current internal value
+         *************************************/
+        private void onValueButtonPush(string numberToAppend)
+        {
+            string displayValue = TextboxTotalDisplay.Text;
+            try
+            {
+                displayValue = m_calc.appendToValue(numberToAppend);
+            }
+            catch (Exception exc)
+            {
+                string messageBoxText = exc.Message;
+                string caption = "Error";
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Error;
+                MessageBoxResult result;
+
+                result = MessageBox.Show(messageBoxText, caption, button, icon);
+                displayValue = m_calc.getDisplayValue();
+            }
+
+            TextboxValueDisplay.Text = displayValue;
+        }
+
+        /**************************************
+         * Handler for ButtonOne
+         *************************************/
+        private void OnPush_ButtonOne(object sender, RoutedEventArgs e)
+        {
+            onValueButtonPush("1");
+        }
+
+        /**************************************
+         * Handler for ButtonTwo
+         *************************************/
+        private void OnPush_ButtonTwo(object sender, RoutedEventArgs e)
+        {
+            onValueButtonPush("2");
+        }
+
+        /**************************************
+         * Handler for ButtonThree
+         *************************************/
+        private void OnPush_ButtonThree(object sender, RoutedEventArgs e)
+        {
+            onValueButtonPush("3");
+        }
+
+        /**************************************
+         * Handler for ButtonFour
+         *************************************/
+        private void OnPush_ButtonFour(object sender, RoutedEventArgs e)
+        {
+            onValueButtonPush("4");
+        }
+
+        /**************************************
+         * Handler for ButtonFive
+         *************************************/
+        private void OnPush_ButtonFive(object sender, RoutedEventArgs e)
+        {
+            onValueButtonPush("5");
+        }
+
+        /**************************************
+         * Handler for ButtonSix
+         *************************************/
+        private void OnPush_ButtonSix(object sender, RoutedEventArgs e)
+        {
+            onValueButtonPush("6");
+        }
+
+        /**************************************
+         * Handler for ButtonSeven
+         *************************************/
+        private void OnPush_ButtonSeven(object sender, RoutedEventArgs e)
+        {
+            onValueButtonPush("7");
+        }
+
+        /**************************************
+         * Handler for ButtonEight
+         *************************************/
+        private void OnPush_ButtonEight(object sender, RoutedEventArgs e)
+        {
+            onValueButtonPush("8");
+        }
+
+        /**************************************
+         * Handler for ButtonNine
+         *************************************/
+        private void OnPush_ButtonNine(object sender, RoutedEventArgs e)
+        {
+            onValueButtonPush("9");
+        }
+
+        /**************************************
+         * Change value from positive to negative or vice versa
+         *************************************/
+        private void OnPush_ButtonPlusNegative(object sender, RoutedEventArgs e)
+        {
+            m_calc.toggleSign();
+            TextboxValueDisplay.Text = m_calc.getDisplayValue();
+        }
+
+        /**************************************
+         * Handler for ButtonZero
+         *************************************/
+        private void OnPush_ButtonZero(object sender, RoutedEventArgs e)
+        {
+            onValueButtonPush("0");
+        }
+
+        /**************************************
+         * Handler for ButtonDecimal
+         *************************************/
+        private void OnPush_ButtonDecimal(object sender, RoutedEventArgs e)
+        {
+            onValueButtonPush(".");
+        }
+
+        /**************************************
+         * Add internal value to the total
+         *************************************/
+        private void OnPush_ButtonPlus(object sender, RoutedEventArgs e)
+        {
+            double displayTotal = m_calc.add();
+            TextboxTotalDisplay.Text = displayTotal.ToString();
+            TextboxValueDisplay.Text = m_calc.getDisplayValue();
+        }
+
+        /**************************************
+         * Subtract internal value from the total
+         *************************************/
+        private void OnPush_ButtonMinus(object sender, RoutedEventArgs e)
+        {
+            double displayTotal = m_calc.subtract();
+            TextboxTotalDisplay.Text = displayTotal.ToString();
+            TextboxValueDisplay.Text = m_calc.getDisplayValue();
+        }
+
+        /**************************************
+         * Multiply total by the current value
+         *************************************/
+        private void OnPush_ButtonMultiply(object sender, RoutedEventArgs e)
+        {
+            double displayTotal = m_calc.multiply();
+            TextboxTotalDisplay.Text = displayTotal.ToString();
+            TextboxValueDisplay.Text = m_calc.getDisplayValue();
+        }
+
+        /**************************************
+        * Divide total by the current value
+        *************************************/
+        private void OnPush_ButtonDivide(object sender, RoutedEventArgs e)
+        {
+            double displayTotal = m_calc.divide();
+            TextboxTotalDisplay.Text = displayTotal.ToString();
+            TextboxValueDisplay.Text = m_calc.getDisplayValue();
+        }
+
+        /**************************************
+        * Clear internal total and value
+        *************************************/
+        private void OnPush_ButtonClear(object sender, RoutedEventArgs e)
+        {
+            double displayTotal = m_calc.clear();
+            TextboxTotalDisplay.Text = displayTotal.ToString();
+            TextboxValueDisplay.Text = m_calc.getDisplayValue();
+        }
+
+        /**************************************
+        * Clear internal value
+        *************************************/
+        private void OnPush_ButtonClearValue(object sender, RoutedEventArgs e)
+        {
+            m_calc.clearValue();
+            TextboxValueDisplay.Text = m_calc.getDisplayValue();
+        }
+    }
+}
